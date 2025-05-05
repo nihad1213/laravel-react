c756dda6054e5c2f164c7b61b81328a5b7b74885
Nihad <nihad.nemetli@gmail.com>
2025-05-05 10:16:44 +0400

    layouts and childrens created and added

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -3,21 +3,36 @@ import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
 import NotFound from "./views/NotFound.jsx";
 import Users from "./views/Users.jsx";
+import DefaultLayout from "./components/DefaultLayout.jsx";
+import GuestLayout from "./components/GuestLayout.jsx";
 
 const router = createBrowserRouter([
+
     {
-        path: "/login",
-        element: <Login />,
+        path: "/",
+        element: <DefaultLayout />,
+        children:[
+            {
+                path: "/users",
+                element: <Users />,
+            },
+        ]
     },
 
     {
-        path: "/signup",
-        element: <Signup />,
-    },
+        path: "/",
+        element: <GuestLayout />,
+        children:[
+            {
+                path: "/login",
+                element: <Login />,
+            },
 
-    {
-        path: "/users",
-        element: <Users />,
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+        ]
     },
 
     {
