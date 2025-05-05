40ff18326f25657993b0e7a482b28b3a03f03e8b
Nihad <nihad.nemetli@gmail.com>
2025-05-05 10:53:42 +0400

    ContextProvider added main.jsx file

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
+
+    const {user, token} = useStateContext();
+
+    if (token) {
+        return <Navigate to="/" />
+    }
+
     return (
         <div>
             <Outlet />
