8d74e972eca50507c5b443fc9c8361fa88593cb7
Nihad <nihad.nemetli@gmail.com>
2025-05-05 10:09:21 +0400

    tailwindcss added to project

--- react/src/views/NotFound.jsx
+++ react/src/views/NotFound.jsx
@@ -1,8 +1,10 @@
 export default function NotFound() {
     return (
-        <div>
-            404 - Not Found
+      <div className="min-h-screen flex items-center justify-center bg-slate-50">
+        <div className="text-center p-8">
+          <h1 className="text-6xl font-bold text-slate-800 mb-2">404</h1>
+          <p className="text-xl text-slate-600">Not Found</p>
         </div>
+      </div>
     );
-}
-
+  }
